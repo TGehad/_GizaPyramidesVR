df4982a4c102b9ad671a0477226c816b4bd041bc
TGehad <Contact.tahagehad@gmail.com>
2022-06-29 10:12:58 +0200

    without assets

---
+++ _PyramidsVR/Assets/scripts/playerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed =10f;
+    bool isMoving=false;
+
+    public Transform[] targets;
+    int nextIndex;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("welcome to egypt");
+        //set player o first position
+        transform.position=targets[0].position;
+        nextIndex=1;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //check input
+        HandleInput();
+       //start move
+        HandleMovement();
+    }
+
+    void HandleInput()
+    {
+       //check for fire1 axix
+       if(Input.GetButtonDown("Fire1"))
+       {
+           //negate ismoving
+           isMoving=!isMoving;
+       }
+
+    }
+     //take care of movement
+    void HandleMovement()
+    {
+        if(!isMoving) return;
+
+        // calc distance from target
+        float dist = Vector3.Distance(transform.position,targets[nextIndex].position);
+        //have we arrive
+        if(dist>0)
+        {
+
+        //calc how much we need move step d=v*t
+        float step =speed * Time.deltaTime;
+        //move that step
+        transform.position=Vector3.MoveTowards(transform.position,targets[nextIndex].position,step);
+
+        }
+        // if we arrive
+        else
+        {
+            nextIndex++;
+
+            if(nextIndex==targets.Length){
+
+                nextIndex=0;
+            }
+            //stop moving
+            isMoving=false;
+
+        }
+
+
+    }
+
+}
---
+++ _PyramidsVR/Assets/scripts/sound.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class sound : MonoBehaviour
+{
+  
+   public AudioSource[] audioSourceObject;
+
+  
+
+
+    void Update()
+    {
+         
+        RaycastHit hit ;
+
+      if(Physics.Raycast(transform.position,transform.forward,out hit)){
+         
+    
+      if(hit.transform.name=="sphinx g")
+        {
+          
+       audioSourceObject[0].Play();
+
+        }
+       
+        if(hit.transform.name=="Gpyramid g")
+        {
+          audioSourceObject[0].Stop();
+         audioSourceObject[1].Play();
+
+        }
+        if(hit.transform.name=="Mpyramid g")
+        {
+       audioSourceObject[1].Stop();
+       audioSourceObject[2].Play();
+
+        }
+       if(hit.transform.name=="Spyramid g")
+        {
+      audioSourceObject[2].Stop();
+       audioSourceObject[3].Play();
+
+        }
+
+      }
+     }
+
+ }
+
